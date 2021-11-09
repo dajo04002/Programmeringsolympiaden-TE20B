ada1321728f9924c86b0635472954a902d4ce1f9
dajo04002 <92377225+dajo04002@users.noreply.github.com>
2021-11-09 15:56:09 +0100

    Add files via upload

---
+++ Upg1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Upg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kuvert= 0.074196;
+            double affish= 0.12474;
+            double information= 0.06237;
+
+            Console.WriteLine("Kuvert ?");
+            int kuvertVikt =int.Parse(Console.ReadLine());
+            Console.WriteLine("Affish ?");
+            int affishVikt =int.Parse(Console.ReadLine());
+            Console.WriteLine("Blad ?");
+            int bladVikt =int.Parse(Console.ReadLine());
+
+            double sum= (kuvertVikt*kuvert*2)+(affishVikt*affish*2)+(bladVikt*information);
+            
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ Upg3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Upg2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Antal med grönt kort?");
+            int M = int.Parse(Console.ReadLine());
+            int s = 0;
+
+            if(M % 2 == 0){
+                s = (M/2)+(2);
+            }
+            else{
+                s = ((M+1)/2)+(2);
+            }
+            Console.WriteLine(s*10);
+
+
+            //2 1 30
+            //2 2 30
+            //2 3 40
+            //2 4 40
+            //2 5 50
+            //2 6 50
+            //2 7 60
+            //2 8 60
+
+        }
+    }
+}
